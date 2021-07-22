6e5baaf6259850356b224d7f7001476763f32892
Mazin Ghafadgi <mazinghafadgi@Mazins-MacBook-Pro-2.local>
2021-07-22 11:08:36 +0100

    started the poc - parsing json

---
+++ src/main/java/io/cdap/plugin/json/parser/JsonParserApp.java
@@ -0,0 +1,44 @@
+package io.cdap.plugin.json.parser;
+
+import com.google.gson.Gson;
+import io.cdap.plugin.proto.Configuration;
+
+
+/*
+{
+  "arguments" : [
+    {
+      "name": "input.path",
+      "value": "gs://reusable-pipeline-tutorial/user-emails.txt"
+    },
+    {
+      "name": "directives",
+      "value": "send-to-error !dq:isEmail(body)"
+    }
+  ]
+}
+ */
+public class JsonParserApp {
+    private static final Gson gson = new Gson();
+    public static void main(String[] args) {
+        // "directives": "split-to-rows body \\n\nparse-as-json :body 1\nparse-as-json :body__id 1\nparse-as-json :body_weeks_on_list 1\nparse-as-json :body_rank_last_week 1\nparse-as-json :body_rank 1\nparse-as-json :body_price 1\nparse-as-json :body_published_date 2\nparse-as-json :body_bestsellers_date 2\ncolumns-replace s/^body_//g\ncleanse-column-names\nrename _id__oid id\nrename weeks_on_list__numberint weeks_on_list\nrename rank_last_week__numberint rank_last_week\nrename rank__numberint rank\nfill-null-or-empty :price__numberint '0'\nrename price__numberint price\nrename published_date__date__numberlong published_date\nrename bestsellers_date__date__numberlong bestsellers_date\nfill-null-or-empty :price__numberdouble '0'\nrename price__numberdouble price_other\nfill-null-or-empty :price '0'\nfill-null-or-empty :price_other '0'\nset-type :price float\nset-type :price_other float\nset-column :price_final price+price_other\ndrop price\ndrop price_other\nset-type :published_date long\nset-type :bestsellers_date long\nset-type :weeks_on_list integer\nset-type :rank_last_week integer\nset-type :rank long\nfilter-rows-on regex-not-match rank_last_week ^0$\ndrop rank_last_week\ndrop weeks_on_list\ndrop published_date\ndrop bestsellers_date\nfilter-rows-on condition-false rank <=3\nfilter-rows-on regex-match price_final ^0.0$\nfilter-rows-on condition-false price_final <25.0",
+
+        String body = "{\n" +
+                "  \"arguments\" : [\n" +
+                "    {\n" +
+                "      \"name\": \"input.path\",\n" +
+                "      \"value\": \"gs://reusable-pipeline-tutorial/user-emails.txt\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"directives\",\n" +
+                "      \"value\": \"send-to-error !dq:isEmail(body)\"\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+        DQConfiguration configuration = gson.fromJson(body, DQConfiguration.class);
+        System.out.println(configuration);
+        //configuration.getArguments();
+}
+
+
+}
